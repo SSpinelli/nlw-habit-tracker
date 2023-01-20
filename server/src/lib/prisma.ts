8404f3d7b353e7cd393a9aa6b74c342e7371f929
João Gabriel Soriano Spinelli <jgsspinelli92@gmail.com>
2023-01-20 16:27:06 -0300

    [NLW] Aula 03 - Criação das rotas summary e habits/:id/toggle.

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient({
-    log: ['query']
-}) // Já da acesso ao banco de dados.
+  log: ["query"],
+}); // Já da acesso ao banco de dados.
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,24 +1,24 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "./lib/prisma";
-import dayjs from "dayjs"
+import dayjs from "dayjs";
 
 export async function appRoutes(app: FastifyInstance) {
   app.get("/", async () => {
-    const habits = await prisma.habit.findMany()
+    const habits = await prisma.habit.findMany();
 
-    return habits
-  })
+    return habits;
+  });
 
-  app.post('/habits', async (req, res) => {
+  app.post("/habits", async (req, res) => {
     const createHabitBody = z.object({
       title: z.string(),
-      weekDays: z.array(z.number().min(0).max(6))
-    })
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
 
-    const { title, weekDays } = createHabitBody.parse(req.body)
+    const { title, weekDays } = createHabitBody.parse(req.body);
 
-    const today = dayjs().startOf('day').toDate()
+    const today = dayjs().startOf("day").toDate();
 
     await prisma.habit.create({
       data: {
@@ -28,24 +28,22 @@ export async function appRoutes(app: FastifyInstance) {
           create: weekDays.map((weekday) => {
             return {
               week_day: weekday,
-            }
-          })
-        }
-      }
-    })
-  })
+            };
+          }),
+        },
+      },
+    });
+  });
 
   app.get("/day", async (req) => {
     const getDayParams = z.object({
-      date: z.coerce.date()
-    })
+      date: z.coerce.date(),
+    });
 
-    const { date } = getDayParams.parse(req.query)
+    const { date } = getDayParams.parse(req.query);
 
-    const parsedDate = dayjs(date).startOf('day')
-    const weekday = parsedDate.get('day')
-
-    console.log(date, weekday)
+    const parsedDate = dayjs(date).startOf("day");
+    const weekday = parsedDate.get("day");
 
     const possibleHabits = await prisma.habit.findMany({
       where: {
@@ -54,32 +52,107 @@ export async function appRoutes(app: FastifyInstance) {
         },
         weekDays: {
           some: {
-            week_day: weekday
-          }
-        }
-      }
-    })
+            week_day: weekday,
+          },
+        },
+      },
+    });
 
-    const day = await prisma.day.findUnique({
+    const day = await prisma.day.findFirst({
       where: {
-        date: parsedDate.toDate()
+        date: parsedDate.toDate(),
       },
       include: {
-        dayHabits: true
-      }
-    })
+        dayHabits: true,
+      },
+    });
 
-    const completedHabits = day?.dayHabits.map((habit) => {
-      return habit.habit_id
-    })
+    const completedHabits = day?.dayHabits.map((dayHabit) => {
+      return dayHabit.habit_id;
+    });
+
+    console.log(day);
 
     return {
       possibleHabits,
-      completedHabits
+      completedHabits,
+    };
+  });
+
+  app.patch("/habits/:id/toggle", async (req, res) => {
+    // Desenvolver a possibilidade de marcar hábitos retroativos
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitParams.parse(req.params);
+
+    const today = dayjs().startOf("day").toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
     }
-  })
 
-  // app.patch("/day", async (req, res) => {
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
 
-  // })
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
+  });
+
+  app.get("/summary", async () => {
+    // Query mais complexa, mais condições, relacionamentos => SQL na mão (RAW)
+    // Prisma ORM: RAW SQL => SQLite
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id,
+        D.date,
+        (
+          SELECT 
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habbit_week_days HWD
+          JOIN habits H
+            ON H.id = HWD.habit_id
+          WHERE
+            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `;
+    return summary;
+  });
 }
