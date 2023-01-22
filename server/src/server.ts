b551f47746ea6509730c7e432398e2fb7014e175
João Gabriel Soriano Spinelli <jgsspinelli92@gmail.com>
2023-01-22 16:18:32 -0300

    [Fix] arruma problema no fatify que não permitia a integração com o mobile

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,14 +6,13 @@ import { appRoutes } from "./routes"
 const app = Fastify()
 
 
-app.register(cors, {
-  origin: ["http://localhost:5173", "http://localhost:3333"]
-}) // Configuração para o Frontend vai poder acessar o Backend.
+app.register(cors) // Configuração para o Frontend vai poder acessar o Backend.
 
 app.register(appRoutes)
 
 app.listen({
-  port: 3333
+  port: 3333,
+  host: "0.0.0.0"
 }).then(() => {
   console.log("HTTP Server running!")
 })
